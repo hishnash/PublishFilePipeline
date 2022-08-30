1fd6d4aa8ee29a453312f79f0b7e81b68867213f
Matthaus Woolard <matthaus.woolard@gmail.com>
2022-08-30 13:48:19 +1200

    update to no longer requires macOS 13

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PublishFilePipeline",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
